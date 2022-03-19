d30b5bdbc54ea2660930240d31407ad856fb9152
georgekarounias <george.karounias@gmail.com>
2022-03-20 00:38:41 +0200

    add main functionality for q2

--- q2/map/taximapq2/handler.py
+++ q2/map/taximapq2/handler.py
@@ -1,7 +1,45 @@
-def handle(req):
-    """handle a request to the function
-    Args:
-        req (str): request body
-    """
+from turtle import distance
+from minio import Minio
+import socket
 
+def SetMC():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    ipaddress = s.getsockname()[0]
+    s.close()
+    mc = Minio(f"{ipaddress}:9000",
+        access_key='minioadmin',
+        secret_key='minioadmin',
+        secure=False)
+    return mc
+
+def GetTrips(req):
+    return
+
+def WriteToBucket(trips):
+    return
+
+def CalcDistance(trip):
+    return
+
+def GetTripDuration(trip):
+    return
+
+def GetPassengers(trip):
+    return
+
+def DoesTripMeetingRequirements(distance, duration, passengers):
+    return
+
+def handle(req):
+    trips = GetTrips(req)
+    tripsMettingReqs = []
+    for trip in trips:
+        distance = CalcDistance(trip)
+        duration = GetTripDuration(trip)
+        passengers = GetPassengers(trip)
+        doesTripMeetingRequirements = DoesTripMeetingRequirements(distance, duration, passengers)
+        if(doesTripMeetingRequirements == True):
+            tripsMettingReqs.append(trip)
+    WriteToBucket(trips) 
     return req
