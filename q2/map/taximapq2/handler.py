d6ea91fdd63d5f80145c4fe1c3eda3302a240467
georgekarounias <george.karounias@gmail.com>
2022-03-20 13:08:24 +0200

    update scripts

--- q2/map/taximapq2/handler.py
+++ q2/map/taximapq2/handler.py
@@ -1,45 +1,46 @@
-from turtle import distance
-from minio import Minio
-import socket
+# from turtle import distance
+# from minio import Minio
+# import socket
 
-def SetMC():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect(("8.8.8.8", 80))
-    ipaddress = s.getsockname()[0]
-    s.close()
-    mc = Minio(f"{ipaddress}:9000",
-        access_key='minioadmin',
-        secret_key='minioadmin',
-        secure=False)
-    return mc
+# def SetMC():
+#     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#     s.connect(("8.8.8.8", 80))
+#     ipaddress = s.getsockname()[0]
+#     s.close()
+#     mc = Minio(f"{ipaddress}:9000",
+#         access_key='minioadmin',
+#         secret_key='minioadmin',
+#         secure=False)
+#     return mc
 
-def GetTrips(req):
-    return
+# def GetTrips(req):
+#     return
 
-def WriteToBucket(trips):
-    return
+# def WriteToBucket(trips):
+#     return
 
-def CalcDistance(trip):
-    return
+# def CalcDistance(trip):
+#     return
 
-def GetTripDuration(trip):
-    return
+# def GetTripDuration(trip):
+#     return
 
-def GetPassengers(trip):
-    return
+# def GetPassengers(trip):
+#     return
 
-def DoesTripMeetingRequirements(distance, duration, passengers):
-    return
+# def DoesTripMeetingRequirements(distance, duration, passengers):
+#     return
 
 def handle(req):
-    trips = GetTrips(req)
-    tripsMettingReqs = []
-    for trip in trips:
-        distance = CalcDistance(trip)
-        duration = GetTripDuration(trip)
-        passengers = GetPassengers(trip)
-        doesTripMeetingRequirements = DoesTripMeetingRequirements(distance, duration, passengers)
-        if(doesTripMeetingRequirements == True):
-            tripsMettingReqs.append(trip)
-    WriteToBucket(trips) 
+    print("Mapper Q2")
+    # trips = GetTrips(req)
+    # tripsMettingReqs = []
+    # for trip in trips:
+    #     distance = CalcDistance(trip)
+    #     duration = GetTripDuration(trip)
+    #     passengers = GetPassengers(trip)
+    #     doesTripMeetingRequirements = DoesTripMeetingRequirements(distance, duration, passengers)
+    #     if(doesTripMeetingRequirements == True):
+    #         tripsMettingReqs.append(trip)
+    # WriteToBucket(trips) 
     return req
