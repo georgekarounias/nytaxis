8cee86b3d797a8811ed4496c7b490c3b7f738e2a
Tasos <tassos.antono96@gmail.com>
2022-03-20 03:06:27 +0200

    read infos from minio request

--- q1/map/taximapq1/handler.py
+++ q1/map/taximapq1/handler.py
@@ -1,7 +1,26 @@
+from fileinput import filename
+from minio import Minio
+from minio.error import S3Error
+import json
+
+def SetMC():
+    mc = Minio("192.168.1.11:9000",
+        access_key='minioadmin',
+        secret_key='minioadmin',
+        secure=False)
+    return mc
+
+def GetMinioInfos(req_dict):
+    keyStrArray = req_dict['Key'].split('/')
+    return keyStrArray[0], keyStrArray[1]
+
 def handle(req):
-    """handle a request to the function
-    Args:
-        req (str): request body
-    """
+    SetMC()
+
+    req_dict = json.loads(req)
+    minioBucketName, minioFileName = GetMinioInfos(req_dict)
+
+    print(minioBucketName)
+    print(minioFileName)
 
     return req
