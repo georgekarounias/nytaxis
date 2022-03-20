a86ac290efe6433fa9e56885be9076f81e8ee9ea
Tasos <tassos.antono96@gmail.com>
2022-03-21 00:30:42 +0200

    read from input, write results to output

--- q1/map/taximapq1/handler.py
+++ q1/map/taximapq1/handler.py
@@ -2,6 +2,11 @@ from fileinput import filename
 from minio import Minio
 from minio.error import S3Error
 import json
+import uuid
+import os
+
+OUTBUCKETNAME = 'map1output'
+
 
 def SetMC():
     mc = Minio("192.168.1.11:9000",
@@ -10,17 +15,55 @@ def SetMC():
         secure=False)
     return mc
 
+mc = SetMC()
+
 def GetMinioInfos(req_dict):
     keyStrArray = req_dict['Key'].split('/')
     return keyStrArray[0], keyStrArray[1]
 
-def handle(req):
-    SetMC()
+def GetDataFromBucketFile(minioBucketName, minioFileName):
+    mc.fget_object(minioBucketName, minioFileName, "/tmp/" + minioFileName)
+    with open("/tmp/" + minioFileName) as json_file:
+        data = json.load(json_file)
+    return data
 
+def ProcessData(data):
+    objects = []
+    for i in data:
+        area = GetArea(i['pickup_longitude'], i['pickup_latitude'])
+        dictionary ={"Id" : i['id'], "Area" : area}
+        objects.append(dictionary)
+    
+    # Serializing json 
+    json_object = json.dumps(objects)
+    WriteToBucket(json_object, OUTBUCKETNAME)
+
+def GetArea(longitude, latitude):
+    if longitude > '-73.935242' and latitude > '40.730610':
+        return 1
+    elif longitude > '-73.935242' and latitude < '40.730610':
+        return 2
+    elif  longitude < '-73.935242' and latitude > '40.730610':
+        return 3
+    else:
+        return 4
+
+def WriteToBucket(jsobj, bucketname):
+    outputfilename = str(uuid.uuid4())+".json"
+    write_json(jsobj, "/tmp/" + outputfilename)
+    mc.fput_object(bucketname, outputfilename, "/tmp/" + outputfilename)
+    os.remove("/tmp/" + outputfilename)
+    return
+
+def write_json(obj, filename):
+    with open(filename, 'w', encoding='utf-8') as jsonf: 
+        json.dump(obj, jsonf) 
+
+def handle(req):
     req_dict = json.loads(req)
     minioBucketName, minioFileName = GetMinioInfos(req_dict)
 
-    print(minioBucketName)
-    print(minioFileName)
+    data = GetDataFromBucketFile(minioBucketName, minioFileName)
+    ProcessData(data)
 
     return req
