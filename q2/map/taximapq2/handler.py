66ea921bd26e08d5afbe8d5a133870a437c61970
georgekarounias <george.karounias@gmail.com>
2022-03-20 17:23:57 +0200

    map q2 print req on bucket

--- q2/map/taximapq2/handler.py
+++ q2/map/taximapq2/handler.py
@@ -1,15 +1,28 @@
+from ipaddress import ip_address
 from turtle import distance
 from minio import Minio
 import socket
 import uuid
+import json
+import os
+
 
 BUCKETNAME = "testbucket"
 
+def GetNetworkIp():
+    # hostname = socket.gethostname()
+    # local_ip = socket.gethostbyname(hostname)
+    # s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # s.connect(("8.8.8.8", 80))
+    # local_ip = s.getsockname()[0]
+    # s.close()
+    local_ip="192.168.42.45"
+    return local_ip
+        
+
 def SetMC():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect(("8.8.8.8", 80))
-    ipaddress = s.getsockname()[0]
-    s.close()
+    ipaddress = GetNetworkIp()
+    print(f"ip: {ipaddress}")
     mc = Minio(f"{ipaddress}:9000",
         access_key='minioadmin',
         secret_key='minioadmin',
@@ -19,15 +32,16 @@ def SetMC():
 # def GetTrips(req):
 #     return
 
-def WriteToBucket(trips, mc, bucketname):
-    # write to temporary file
-    file_name = str(uuid.uuid4())
-    f = open("/tmp/" + file_name, "a")
-    f.write(trips)
-    f.close()
+def write_json(obj, filename):
+    with open(filename, 'w', encoding='utf-8') as jsonf: 
+        json.dump(obj, jsonf) 
 
-    # sync to Minio
-    mc.fput_object(bucketname, file_name, "/tmp/"+file_name)
+def WriteToBucket(trips, mc, bucketname):
+    #trips['name']=req['Key']
+    filename = str(uuid.uuid4())+".json" #req['Key'].split("/")[1]
+    write_json(trips, filename)
+    mc.fput_object(bucketname, filename, filename)
+    os.remove(filename)
     return
 
 # def CalcDistance(trip):
@@ -55,7 +69,6 @@ def handle(req):
     #     if(doesTripMeetingRequirements == True):
     #         tripsMettingReqs.append(trip)
     # WriteToBucket(trips, mc, "testbucket") 
-    print("Trying write to bucket")
-    WriteToBucket(req, mc, BUCKETNAME) 
-    print("Wrote to bucket")
+    x="{\"name\": \"geo\"}"
+    WriteToBucket(json.loads(req), mc, BUCKETNAME) 
     return req
